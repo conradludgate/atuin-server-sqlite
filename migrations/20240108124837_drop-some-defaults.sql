131591b247285b53f9f6bc5948fab3eff3a7e5dd
Conrad Ludgate <conradludgate@gmail.com>
2024-12-22 08:26:46 +0000

    fix migrations

--- migrations/20240108124837_drop-some-defaults.sql
+++ migrations/20240108124837_drop-some-defaults.sql
@@ -1,4 +1,5 @@
--- Add migration script here
-alter table history alter column user_id drop default;
-alter table sessions alter column user_id drop default;
-alter table total_history_count_user alter column user_id drop default;
+-- deleting default values from columns is not supported in sqlite.
+
+-- alter table history alter column user_id drop default;
+-- alter table sessions alter column user_id drop default;
+-- alter table total_history_count_user alter column user_id drop default;
